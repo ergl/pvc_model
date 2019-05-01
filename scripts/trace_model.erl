6a40228d9374fe8bbce50c09d201dff2f33b78a4
ergl <ergl@users.noreply.github.com>
2019-05-01 18:12:22 +0200

    Simplify simulator trace

--- scripts/trace_model.erl
+++ scripts/trace_model.erl
@@ -1,32 +1,32 @@
-Ring = pvc_model:start_partitions([0,1,2,3,4,5,6,7]).
+rr("src/pvc_model_partition_owner.erl").
+rr("src/pvc_commit_log.erl").
+rr("src/pvc_commit_queue.erl").
+rr("src/pvc_version_log.erl").
+rr("src/pvc_client.erl").
+
+Ring = pvc_model:start_partitions([0,1,2]).
 Client = pvc_client:new(Ring).
 
-% Tx a
-Tx00 = pvc_client:start_transaction(tx_a).
-{ok, _, Tx01} = pvc_client:read([4], Tx00, Client).
-Tx02 = pvc_client:update([{4, 0}], Tx01, Client).
-pvc_client:commit(Tx02).
+% Tx A
+TxA0 = pvc_client:start_transaction(tx_a).
+{ok, _, TxA1} = pvc_client:read([0,1], TxA0, Client).
+TxA2 = pvc_client:update([{0,0},{1,0}], TxA1, Client).
 
-% Tx b
-Tx10 = pvc_client:start_transaction(tx_b).
-{ok, _, Tx11} = pvc_client:read([0,4], Tx10, Client).
-Tx12 = pvc_client:update([{0,0},{4,0}], Tx11, Client).
+% Tx B, prepare and get stuck
+TxB0 = pvc_client:start_transaction(tx_b).
+{ok, _, TxB1} = pvc_client:read([2], TxB0, Client).
+TxB2 = pvc_client:update([{2,0}], TxB1, Client).
+_ = pvc_client:prepare(TxB2).
 
-% Tx c, prepare and get stuck
-Tx20 = pvc_client:start_transaction(tx_c).
-{ok, _, Tx21} = pvc_client:read([1], Tx20, Client).
-Tx22 = pvc_client:update([{1,0}], Tx21, Client).
-_ = pvc_client:prepare(Tx22).
+% Tx C runs to completion
+TxC0 = pvc_client:start_transaction(tx_c).
+{ok, _, TxC1} = pvc_client:read([1,2], TxC0, Client).
+TxC2 = pvc_client:update([{4,0}, {5,0}], TxC1, Client).
+pvc_client:commit(TxC2).
 
-% Tx d runs entirely here
-Tx30 = pvc_client:start_transaction(tx_d).
-{ok, _, Tx31} = pvc_client:read([1, 4], Tx30, Client).
-Tx32 = pvc_client:update([{9,0}, {12,0}], Tx31, Client).
-pvc_client:commit(Tx32).
+% Tx A commits here
+ok = pvc_client:commit(TxA2).
 
-% Tx b commits here
-ok = pvc_client:commit(Tx12).
-
-% Tx e
-Tx40 = pvc_client:start_transaction(tx_e).
-{error, abort_read} = pvc_client:read([0,1,4], Tx40, Client).
+% Tx D
+TxD0 = pvc_client:start_transaction(tx_d).
+{error, abort_read} = pvc_client:read([0,2,1], TxD0, Client).
--- scripts/trace_simulator.erl
+++ scripts/trace_simulator.erl
@@ -1,31 +1,24 @@
-Ring = pvc_model:start_partitions([0,1,2,3,4,5,6,7]).
+Ring = pvc_model:start_partitions([0,1,2]).
 Client = pvc_client:new(Ring).
 
 Prefix = [
-  % Transaction A
+  % Transaction A reads and updates, inits prepare
   #{id => tx_a,op => start_tx},
-  #{id => tx_a,op => {read,[4]}},
-  #{id => tx_a,op => {update,[{4,0}]}},
+  #{id => tx_a,op => {read,[2]}},
+  #{id => tx_a,op => {update,[{2,0}]}},
   #{id => tx_a,op => prepare},
-  #{id => tx_a,op => commit},
 
   % Transaction B, reads and updates, but does not start commit
   #{id => tx_b,op => start_tx},
-  #{id => tx_b,op => {read,[0,4]}},
-  #{id => tx_b,op => {update,[{0,0},{4,0}]}},
+  #{id => tx_b,op => {read,[0,1]}},
+  #{id => tx_b,op => {update,[{0,0},{1,0}]}},
 
-  % Transaction C reads and updates, inits prepare
+  % Transaction C runs to completion
   #{id => tx_c,op => start_tx},
-  #{id => tx_c,op => {read,[1]}},
-  #{id => tx_c,op => {update,[{1,0}]}},
+  #{id => tx_c,op => {read,[1,2]}},
+  #{id => tx_c,op => {update,[{4,0},{5,0}]}}, %% 4 and 5 fall in 1 and 2
   #{id => tx_c,op => prepare},
-
-  % Transaction D runs entirely here
-  #{id => tx_d,op => start_tx},
-  #{id => tx_d,op => {read,[1,4]}},
-  #{id => tx_d,op => {update,[{9,0},{12,0}]}}, %% 9 and 12 fall in 1 and 4
-  #{id => tx_d,op => prepare},
-  #{id => tx_d,op => commit},
+  #{id => tx_c,op => commit},
 
   % Transaction B commits here
   #{id => tx_b,op => prepare},
@@ -33,11 +26,11 @@ Prefix = [
 
 [] = history_executor:materialize_execution(Prefix, Client).
 
-Critical = [#{id => tx_e,op => start_tx},
-            #{id => tx_e,op => {read,[0,1,4]}}].
+Critical = [#{id => tx_d,op => start_tx},
+            #{id => tx_d,op => {read,[0,2,1]}}].
 
 history_executor:materialize_execution(Critical, Client).
 
-%% Result: [{tx_e,{read,[0,1,4]},{error,abort_read}}]
+%% Result: [{tx_d,{read,[0,2,1]},{error,abort_read}}]
 
 pvc_model:stop_partitions(Ring), f().
